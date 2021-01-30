ea8557e46e199bbb2d999f8be125063ec8d9521f
Weslley Carneiro <weslley@outlook.com>
2021-01-30 16:34:26 -0300

    Alterando migrations

---
+++ playground/Optsol.Playground.Infra/Migrations/20210130193137_AlerandoCampoCreateDateParaCreatedDate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Optsol.Playground.Infra.Migrations
+{
+    public partial class AlerandoCampoCreateDateParaCreatedDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreateDate",
+                table: "Cliente",
+                newName: "CreatedDate");
+
+            migrationBuilder.RenameColumn(
+                name: "CreateDate",
+                table: "CartaoCredito",
+                newName: "CreatedDate");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreatedDate",
+                table: "Cliente",
+                newName: "CreateDate");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedDate",
+                table: "CartaoCredito",
+                newName: "CreateDate");
+        }
+    }
+}
