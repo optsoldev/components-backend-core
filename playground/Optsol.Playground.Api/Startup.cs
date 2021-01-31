e411b6b31d1d14036a4615c88d1f2214b45976b0
Weslley Carneiro <weslley@outlook.com>
2021-01-31 00:46:11 -0300

    criando resolver para ignorar os fields do domain notifications

--- playground/Optsol.Playground.Api/Startup.cs
+++ playground/Optsol.Playground.Api/Startup.cs
@@ -1,18 +1,19 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Optsol.Playground.Application.Services.Cliente;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using Optsol.Playground.Application.Mappers.Cliente;
+using Optsol.Playground.Application.Services.Cliente;
 using Optsol.Playground.Domain.Repositories.Cliente;
 using Optsol.Playground.Infra.Data.Context;
 using Optsol.Playground.Infra.Data.Repositories.Cliente;
-using Microsoft.AspNetCore.Http;
-using System.Text.Json.Serialization;
-using System;
-using System.Text.Json;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace Optsol.Playground.Api
 {
@@ -32,16 +33,13 @@ namespace Optsol.Playground.Api
         {
             var stringConnection = this.Configuration.GetSection("ConnectionStrings:DefaultConnection");
 
-            services.AddControllers();
+            services.AddControllers().ConfigureNewtonsoftJson();
             services.AddContext<PlaygroundContext>(new ContextOptionsBuilder(stringConnection.Value, "Optsol.Playground.Infra", Environment.IsDevelopment()));
             services.AddRepository<IClienteReadRepository, ClienteReadRepository>("Optsol.Playground.Domain", "Optsol.Playground.Infra");
             services.AddApplicationServices<IClienteServiceApplication, ClienteServiceApplication>("Optsol.Playground.Application");
-
             services.AddAServices();
             services.AddAutoMapper(typeof(ClienteViewModelToEntityMapper));
-
             services.AddSwagger(Configuration);
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
