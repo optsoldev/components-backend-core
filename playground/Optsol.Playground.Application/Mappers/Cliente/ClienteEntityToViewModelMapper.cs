a6863e3a79d7e9467e22bfda844c472ce75eb955
Weslley Carneiro <weslley@outlook.com>
2021-02-01 13:27:38 -0300

    Alteração viewmodels

--- playground/Optsol.Playground.Application/Mappers/Cliente/ClienteEntityToViewModelMapper.cs
+++ playground/Optsol.Playground.Application/Mappers/Cliente/ClienteEntityToViewModelMapper.cs
@@ -9,7 +9,7 @@ namespace Optsol.Playground.Application.Mappers.Cliente
         public ClienteEntityToViewModelMapper()
         {
             CreateMap<ClienteEntity, ClienteViewModel>()
-                .ForMember(item => item.Sobrenome, item => item.MapFrom(src => src.Nome.SobreNome));
+                                .ForMember(item => item.DataCriacao, item => item.MapFrom(src => src.CreatedDate));
 
             CreateMap<ClienteEntity, ClienteComCartoesViewModel>()
                 .ForMember(item => item.Sobrenome, item => item.MapFrom(src => src.Nome.SobreNome));
