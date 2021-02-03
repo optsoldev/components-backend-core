d8ab999346064e2baf3882807321ddad931163a1
Weslley Carneiro <weslley@outlook.com>
2021-02-02 21:03:27 -0300

    [Fact(Skip ="Integração com mongodb somente local")]

--- src/Optsol.Components.Test.Unit/Infra/MongoDB/MongoRepositorySpec.cs
+++ src/Optsol.Components.Test.Unit/Infra/MongoDB/MongoRepositorySpec.cs
@@ -16,7 +16,7 @@ namespace Optsol.Components.Test.Unit.Infra.MongoDB
 {
     public class MongoRepositorySpec
     {
-        [Fact]
+        [Fact(Skip = "Integração com mongodb somente local")]
         public void Deve_Registrar_Logs_No_Repositorio_MongoDB()
         {
             //Given
