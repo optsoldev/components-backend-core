852e12acb6779e1071b0b6979c1f6ad9552b0a5c
Weslley Carneiro <weslley.carneiro@optsol.com.br>
2021-03-07 14:10:48 -0300

    Feature/wc multitenant (#33)
    * alterações no storage
    
    * Ajustes na organização de pastas
    
    * Ajustes nos namespace pos reorganização
    
    * Desenvolvimento da funcionalidade multitenant
    
    * ajustes nos testes automatizados
    
    * Alterando ILogger para ILoggerFactory nas injeções de dependências
    
    * ajustes nos testes automatizados
    
    * ajustes nos testes
    
    * ajustes nos paths
    
    * skipando teste
    
    * ajustes EntityConfigurationBase no HasQueryFilter

--- src/Optsol.Components.Infra/Data/EntityConfigurationBase.cs
+++ src/Optsol.Components.Infra/Data/EntityConfigurationBase.cs
@@ -2,8 +2,11 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Optsol.Components.Domain.Entities;
 using Optsol.Components.Infra.Data.Provider;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using static Optsol.Components.Shared.Extensions.PredicateBuilderExtensions;
 
 namespace Optsol.Components.Infra.Data
 {
@@ -34,23 +37,36 @@ namespace Optsol.Components.Infra.Data
                 .HasColumnType("datetime")
                 .IsRequired();
 
+            LambdaExpression expression = null;
+            var parametrer = Expression.Parameter(typeof(TEntity), "entity");
+
             if (typeof(TEntity).GetInterfaces().Contains(typeof(IDeletable)))
             {
+                expression = CreateInitialFilter(expression);
+
                 builder
                     .Property(entity => ((IDeletable)entity).IsDeleted)
                     .IsRequired();
 
                 builder.Property(entity => ((IDeletable)entity).DeletedDate);
 
-                builder.HasQueryFilter(entity => !((IDeletable)entity).IsDeleted);
+                var member = Expression.Property(parametrer, "IsDeleted");
+                var constant = Expression.Constant(false);
+                var body = Expression.Equal(member, constant);
+
+                var deletableExpression = Expression.Lambda<Func<TEntity, bool>>(body, parametrer);
+
+                expression = Expression.Lambda<Func<TEntity, bool>>(Expression.AndAlso(expression.Body, deletableExpression.Body), deletableExpression.Parameters);
             }
 
             if (typeof(TEntity).GetInterfaces().Contains(typeof(ITenant<TKey>)))
             {
+                expression = CreateInitialFilter(expression);
+
                 builder
                     .Property(entity => ((ITenant<TKey>)entity).TenantId)
                     .IsRequired();
-                
+
                 builder
                     .HasIndex("TenantId")
                     .IsUnique()
@@ -59,9 +75,26 @@ namespace Optsol.Components.Infra.Data
                 var tenantProviderNotIsNull = _tenantProvider != null;
                 if (tenantProviderNotIsNull)
                 {
-                    builder.HasQueryFilter(entity => EqualityComparer<TKey>.Default.Equals(((ITenant<TKey>)entity).TenantId, _tenantProvider.GetTenantId()));
+                    var member = Expression.Property(parametrer, "TenantId");
+                    var constant = Expression.Constant(_tenantProvider.GetTenantId());
+                    var body = Expression.Equal(member, constant);
+
+                    var deletableExpression = Expression.Lambda<Func<TEntity, bool>>(body, parametrer);
+
+                    expression = Expression.Lambda<Func<TEntity, bool>>(Expression.AndAlso(expression.Body, deletableExpression.Body), deletableExpression.Parameters);
                 }
             }
+
+            var hasExpressionFilter = expression != null;
+            if (hasExpressionFilter)
+            {
+                builder.HasQueryFilter(expression);
+            }
+        }
+
+        private LambdaExpression CreateInitialFilter(LambdaExpression expression)
+        {
+            return expression ?? PredicateBuilder.True<TEntity>();
         }
     }
 }
