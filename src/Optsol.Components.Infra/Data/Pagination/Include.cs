c5f0ac52bb4f877aa06c18116c66ab3d12def7a0
Weslley Carneiro <weslley.carneiro@optsol.com.br>
2021-04-12 16:21:28 -0300

    Develop (#53)
    v 1.12.7

---
+++ src/Optsol.Components.Infra/Data/Pagination/Include.cs
@@ -0,0 +1,22 @@
+﻿using Optsol.Components.Domain.Entities;
+using System;
+using System.Linq;
+
+namespace Optsol.Components.Infra.Data.Pagination
+{
+    public class Include<TEntity> : IInclude<TEntity>
+        where TEntity : IEntity
+    {
+        private readonly Func<IQueryable<TEntity>, IQueryable<TEntity>> Includes;
+
+        public Include(Func<IQueryable<TEntity>, IQueryable<TEntity>> expression)
+        {
+            Includes = expression;
+        }
+                
+        public Func<IQueryable<TEntity>, IQueryable<TEntity>> GetInclude()
+        {
+            return this.Includes;
+        }
+    }
+}
