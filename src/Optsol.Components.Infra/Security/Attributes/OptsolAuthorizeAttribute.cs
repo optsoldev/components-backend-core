1fa95331aa33e7be5b1766d347c04c137c42db0f
Weslley Carneiro <weslley@outlook.com>
2021-07-13 09:26:28 -0300

    desenvolvimento

--- src/Optsol.Components.Infra/Security/Attributes/OptsolAuthorizeAttribute.cs
+++ src/Optsol.Components.Infra/Security/Attributes/OptsolAuthorizeAttribute.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Logging;
@@ -6,6 +8,7 @@ using Optsol.Components.Shared.Exceptions;
 using Optsol.Components.Shared.Settings;
 using System;
 using System.Linq;
+using System.Net.Http;
 
 namespace Optsol.Components.Infra.Security.Attributes
 {
@@ -64,7 +67,30 @@ namespace Optsol.Components.Infra.Security.Attributes
 
         private void ContextRemoteSecurity(AuthorizationFilterContext context)
         {
-            //ToDo: Chamar api de claims
+            var accessToken = context.HttpContext.GetTokenAsync("Bearer", "access_token")
+                .GetAwaiter()
+                .GetResult();
+
+            var client = new HttpClient();
+            client.SetBearerToken(accessToken);
+
+            var response = client
+                .GetUserInfoAsync(new UserInfoRequest
+                {
+                    Address = $"{_securitySettings.Authority}/api/usuario/token/has-accesses",
+                    Token = accessToken,
+                })
+                .GetAwaiter()
+                .GetResult();
+
+            // && c.Value.Equals(_claim, StringComparison.OrdinalIgnoreCase)
+            var userAuthenticateHasClaim = response.Claims.Any(c => c.Type.Equals("optsol"));
+            if (userAuthenticateHasClaim)
+            {
+                return;
+            }
+
+            context.Result = new UnauthorizedResult();
         }
     }
 }
