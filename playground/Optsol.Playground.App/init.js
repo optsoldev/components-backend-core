ddccc3c8e0e1b2dd8076e5a474bd044711a41c60
Weslley Carneiro <weslley.carneiro@optsol.com.br>
2021-07-26 12:39:17 -0300

    Develop (#83)
    v1.19.0

---
+++ playground/Optsol.Playground.App/init.js
@@ -0,0 +1,8 @@
+firebase.initializeApp({
+  apiKey: 'AIzaSyDzIDRvzO1CbexVQeXHUtovoofACg1euGI',
+  authDomain: 'component-e95de.firebaseapp.com',
+  projectId: 'component-e95de',
+  storageBucket: 'component-e95de.appspot.com',
+  messagingSenderId: '160192209694',
+  appId: '1:160192209694:web:6e8b5759f14aa664b89f0f',
+});
---
+++ src/Optsol.Components.Infra.Firebase/Clients/IQueryClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optsol.Components.Infra.Firebase.Clients
+{
+    public interface IQueryClient
+    {
+        Expression<Func<T, bool>> Query<T>() where T : IClient;
+    }
+}
