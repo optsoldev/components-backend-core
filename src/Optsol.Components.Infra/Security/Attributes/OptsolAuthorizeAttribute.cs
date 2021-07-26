7c3e61df2ad6ff01ac0d94d1e43990d65e6fe917
Weslley Carneiro <weslley@outlook.com>
2021-07-25 21:04:38 -0300

    removendo namespace

--- src/Optsol.Components.Infra/Security/Attributes/OptsolAuthorizeAttribute.cs
+++ src/Optsol.Components.Infra/Security/Attributes/OptsolAuthorizeAttribute.cs
@@ -1,6 +1,4 @@
-﻿using IdentityModel.Client;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Logging;
@@ -9,7 +7,6 @@ using Optsol.Components.Shared.Exceptions;
 using Optsol.Components.Shared.Settings;
 using System;
 using System.Linq;
-using System.Net.Http;
 using System.Net.Http.Headers;
 
 namespace Optsol.Components.Infra.Security.Attributes
